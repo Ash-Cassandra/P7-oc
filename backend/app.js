df88d42e7b2889e8694e4f9c6be8256c5efeae3b
Ash-Cassandra <c.larcheveque@orange.fr>
2023-05-22 15:51:59 +0200

    ajout fichier routes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const Book =  require('./models/Book');
+const bookRoutes = require('./routes/book')
 const app = express();
 
 mongoose.connect('mongodb+srv://user1:user1@ash.vxla1es.mongodb.net/?retryWrites=true&w=majority',
@@ -19,46 +19,7 @@ app.use((req, res, next) => {
     next();
   });
 
-app.post('/api/books', (req, res, next) => {
-    delete req.body._id;
-    const book = new Book({
-        ...req.body
-    });
-    book.save()
-        .then(() => res.status(201).json({ message: 'livre enregistré'}))
-        .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/books', bookRoutes);
 
-app.get('/api/books', (req, res, next) => {
-    const book = [
-        {
-            "id": "1",
-  "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
-  "title" : "Milwaukee Mission",
-  "author": "Elder Cooper",
-  "imageUrl" : "https://via.placeholder.com/206x260",
-  "year" : 2021,
-  "genre" : "Policier",
-  "ratings" : [{
-    "userId" : "1",
-    "grade": 5
-  },
-    {
-      "userId" : "1",
-      "grade": 5
-    },
-    {
-      "userId" : "clc4wj5lh3gyi0ak4eq4n8syr",
-      "grade": 5
-    },
-    {
-      "userId" : "1",
-      "grade": 5
-    }],
-  "averageRating": 3
-        }
-    ];
-    res.status(200).json(book);
-})
 
 module.exports = app;
